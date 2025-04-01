a73fd20aa6a29366dda467fc4decf3e07648f569
Neil Francis Palmares <nnneninl@proton.me>
2025-04-02 02:42:15 +0800

    feat: add Experience section

---
+++ src/data/experiences.ts
@@ -0,0 +1,16 @@
+export const experiences = [
+  {
+    date: "September, 2024 - December, 2024",
+    title: "Intern - Construction Safety and Health Planning",
+    company: "Department of Labor and Employment - Region 6",
+    description:
+      "During my internship at the Department of Labor and Employment - Region 6, I was assigned to the Construction Safety and Health Program (CSHP) unit. I provided general front desk support, assisting clients and answering inquiries. My responsibilities included reviewing and ensuring completeness of submitted CSHP documents, as well as maintaining and updating detailed records of CSHP applications.",
+  },
+  {
+    date: "February, 2024 - May, 2024",
+    title: "On-the-Job Trainee - IT & Network Operations",
+    company: "Office of Civil Defense - Iloilo Provincial Capitol - Region 6",
+    description:
+      "During my On-The-Job training, I gained hands-on experience in IT and network operations, including the installation of CCTV cameras, setting up routers and PhoneLink, and configuring multiple displays into a single system unit. I also worked on network infrastructure tasks such as Ethernet cable crimping, NAS interface familiarization, and internet installation. Additionally, I developed skills in ArcGIS basics and geo-tracking, as well as OS installation for both Linux and Windows systems.",
+  },
+];
