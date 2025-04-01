487c8d07ce60d1e7034bb00e03af540bac5f0e87
Neil Francis Palmares <nnneninl@proton.me>
2025-04-02 03:44:35 +0800

    feat: add Languages and Tools section

---
+++ src/data/languages-and-tools.ts
@@ -0,0 +1,86 @@
+export const languagesAndTools = [
+  {
+    src: "/icons/typescript.svg",
+    name: "TypeScript",
+  },
+  {
+    src: "/icons/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    src: "/icons/go.svg",
+    name: "Go",
+  },
+  {
+    src: "/icons/react.svg",
+    name: "React.js",
+  },
+  {
+    src: "/icons/next.svg",
+    name: "Next.js",
+  },
+  {
+    src: "/icons/astro.svg",
+    name: "Astro",
+  },
+  {
+    src: "/icons/aws.svg",
+    name: "AWS",
+  },
+  {
+    src: "/icons/python.svg",
+    name: "Python",
+  },
+  {
+    src: "/icons/csharp.svg",
+    name: "C#",
+  },
+  {
+    src: "/icons/html.svg",
+    name: "HTML5",
+  },
+  {
+    src: "/icons/css.svg",
+    name: "CSS3",
+  },
+  {
+    src: "/icons/tailwindcss.svg",
+    name: "Tailwind CSS",
+  },
+  {
+    src: "/icons/mysql.svg",
+    name: "MySQL",
+  },
+  {
+    src: "/icons/postgresql.svg",
+    name: "PostgreSQL",
+  },
+  {
+    src: "/icons/git.svg",
+    name: "Git",
+  },
+  {
+    src: "/icons/github.svg",
+    name: "GitHub",
+  },
+  {
+    src: "/icons/neovim.svg",
+    name: "Neovim",
+  },
+  {
+    src: "/icons/vscode.svg",
+    name: "Visual Studio Code",
+  },
+  {
+    src: "/icons/arch.svg",
+    name: "Arch Linux",
+  },
+  {
+    src: "/icons/debian.svg",
+    name: "Debian Linux",
+  },
+  {
+    src: "/icons/windows.svg",
+    name: "Windows",
+  },
+];
