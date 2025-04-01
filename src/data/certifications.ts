e5844fd7307324d4febbee21cb3b9c0c8cb34695
Neil Francis Palmares <nnneninl@proton.me>
2025-04-02 04:13:42 +0800

    feat: add Certifications section

---
+++ src/data/certifications.ts
@@ -0,0 +1,22 @@
+export const certifications = [
+  {
+    title: "Commission of Civil Service Professional",
+    date: "October 15, 2024",
+    href: "/docs/cert-csc.pdf",
+  },
+  {
+    title: "Cybersecurity Essentials",
+    date: "December 17, 2023",
+    href: "/docs/cert-cybersecurity.pdf",
+  },
+  {
+    title: "Introduction to IoT and Digital Transformation",
+    date: "July 21, 2023",
+    href: "/docs/cert-iot.pdf",
+  },
+  {
+    title: "IT Essentials: PC Hardware and Software",
+    date: "May 20, 2022",
+    href: "/docs/cert-it.pdf",
+  },
+];
