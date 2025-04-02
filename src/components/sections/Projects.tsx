93b8e0c4f12cc709ae91fc75fcecf36a32f16f70
Neil Francis Palmares <nnneninl@proton.me>
2025-04-02 17:00:05 +0800

    feat: add Projects section

---
+++ src/components/sections/Projects.tsx
@@ -0,0 +1,84 @@
+// local
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { projects } from "@/data/projects";
+
+const Projects = () => {
+  return (
+    <>
+      <style>
+        {`
+          @keyframes slideUp {
+            from {
+              opacity: 0;
+              transform: translateY(50px);
+            }
+            to {
+              opacity: 1;
+              transform: translateY(0);
+            }
+          }
+          .slide-up {
+            animation: slideUp 1s ease-out forwards;
+          }
+        `}
+      </style>
+
+      <section className="space-y-8 mb-12 slide-up">
+        <h2 className="text-3xl font-semibold bg-gradient-to-r from-[#d0486e] to-[#7560c9] bg-clip-text text-transparent inline-block">
+          Projects
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {projects.map((project, index) => (
+            <Dialog key={`${project.title}-${index}`}>
+              <DialogTrigger>
+                <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                  <CardContent className="p-4 space-y-4">
+                    <img
+                      src={project.image}
+                      alt={`${project.title} image`}
+                      className="w-full h-30"
+                    />
+
+                    <div className="flex-col items-center justify-between space-y-2">
+                      <h3 className="font-medium">{project.title}</h3>
+                      <div className="flex flex-wrap gap-2">
+                        {project.stack.map((tech, index) => (
+                          <span
+                            key={`${tech}-${index}`}
+                            className="bg-gray-200 dark:bg-gray-700 text-sm px-2 py-1 rounded-md"
+                          >
+                            {tech}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </DialogTrigger>
+
+              <DialogContent>
+                <DialogHeader className="gap-4">
+                  <DialogTitle className="self-center">
+                    {project.title}
+                  </DialogTitle>
+                  <DialogDescription>{project.summary}</DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Projects;
---
+++ src/data/projects.ts
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "My Portfolio",
+    stack: ["Astro", "AWS", "Tailwind CSS"],
+    image: "/images/project-my-portfolio.png",
+    summary:
+      "My personal portfolio website built with Astro and styled using Tailwind CSS. Hosted on AWS, this site showcases my skills, experience, and projects in an optimized, lightweight, and modern design.",
+    link: "",
+  },
+  {
+    title: "Archie",
+    stack: ["Shell Script", "Arch Linux"],
+    image: "/images/placeholder-project.png",
+    summary:
+      "A custom Arch Linux installation script tailored to my personal setup but open for others to use. It automates installation with a Btrfs filesystem (LUKS-encrypted), systemd-boot, zramd, PipeWire, Hyprland, and other optimizations.",
+    link: "",
+  },
+  {
+    title: "Debbie",
+    stack: ["Shell Script", "Debian Linux"],
+    image: "/images/placeholder-project.png",
+    summary:
+      "A post-installation setup script for Debian 12 (Bookworm) that automates the installation and configuration of Nextcloud, Syncthing, and GeoServer—ideal for self-hosted cloud storage and geospatial applications.",
+    link: "",
+  },
+  {
+    title: "Dotfiles",
+    stack: ["Lua", "Shell", "Linux"],
+    image: "/images/placeholder-project.png",
+    summary:
+      "My personal collection of dotfiles, featuring custom configurations for Neovim, Tmux, and other essential development tools. Designed for efficiency, minimalism, and portability across Linux systems.",
+    link: "https://github.com/nnneninl/dotfiles",
+  },
+  {
+    title: "Elden Hoard",
+    stack: ["Next.js", "Tailwind CSS"],
+    image: "/images/placeholder-project.png",
+    summary:
+      "A full-stack Next.js application for tracking collectibles in Elden Ring, including items from the DLC. Features a sleek UI with checklists, item images, and exact in-game locations.",
+    link: "",
+  },
+];
